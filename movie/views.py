41ff92123f0a981cbe86e9bbfc73c143bd9936ee
Smonto-06 <smontoyae5@eafit.edu.co>
2026-08-01 16:04:40 -0500

    nuevo archivo about.html

--- movie/views.py
+++ movie/views.py
@@ -7,4 +7,4 @@ def home(request):
     #return render(request, 'home.html')
     return render(request, 'home.html', {'name': 'Samuel Montoya'})
 def about(request):
-    return HttpResponse('<h1>Welcome to About Page</h1>') 
+    return render(request, "about.html")
