56a3eea93c9221b43702a5d89a4857238e350f73
Daniel Giraldo <dmgiraldom@eafit.edu.co>
2026-08-03 23:42:17 -0500

    busqueda de peliculas

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='movie/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+
 # Create your views here.
-def home(request):
+#def home(request):
     #return HttpResponse("Hello, welcome to home page")
     #return render(request, 'home.html')
-    return render(request, 'home.html', {'name': 'Daniel Giraldo'})
+    #return render(request, 'home.html', {'name': 'Daniel Giraldo'})
+    #searchTerm = request.GET.get('searchMovie')
+    #movies = Movie.objects.all()
+    #return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies, 'name': 'Daniel Giraldo'})
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {
+        'searchTerm': searchTerm,
+        'movies': movies,
+        'name': 'Daniel Giraldo'
+    })
+
+def about(request):
+    return render(request, 'about.html')
