9ce0c5d0fe2d3540bf3dd115b8f99b8c584ca717
Smonto-06 <smontoyae5@eafit.edu.co>
2026-08-26 10:38:29 -0500

    navbar y footer

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'moviereviews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'moviereviews' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,5 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'moviereviews' / 'static']
+
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
