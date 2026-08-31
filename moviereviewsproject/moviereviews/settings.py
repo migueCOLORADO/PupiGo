96eeea0f47ddc75cf7e2bf29f416f7bf3b9083cc
Kiler56 <jpenao@eafit.edu.co>
2026-08-30 21:28:31 -0500

    manejo de archivos estaticos

--- moviereviewsproject/moviereviews/settings.py
+++ moviereviewsproject/moviereviews/settings.py
@@ -129,3 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "moviereviews/static"),
+]
